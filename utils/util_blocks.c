2cba110d86300ab677b600706b51dffdc35868fa
Tataru Tudor Sorin <rodut11@protonmail.com>
2025-09-24 20:27:22 +0300

    added comments
    about damn time

--- utils/util_blocks.c
+++ utils/util_blocks.c
@@ -9,20 +9,28 @@ void grid_print(int grid[MAX_GRID_HEIGHT][MAX_GRID_WIDTH]) {
         printf(" %d |", i);
     }
     printf("\n");
+    //loop through the grid
     for (int i = 0; i < MAX_GRID_HEIGHT; i++) {
         for (int j = 0; j < MAX_GRID_WIDTH; j++) {
+            // if the cell at position i, j is 1, set it to blue and print it
             if (grid[i][j] == 1) {
                 printf(BLU " %d " COLOR_RESET "|", grid[i][j]);
+                // else, instead of 0 print .
             }else
                 printf(" . |");
         }
-        printf(" %d \n", i); // new line after each row
+        printf(" %d \n", i); // print row index at the end of the row
     }
 }
 
+
+// clear grid
 void clear_grid(int grid[8][8]) {
+
+    //loop through the grid
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
+            //set each cell to 0
             grid[i][j] = 0;
         }
     }
@@ -41,21 +49,25 @@ void place_block(int grid[8][8], block b, int pivotX, int pivotY) {
 
     if (!check_collision(grid, b, pivotX, pivotY))
         return; // abort if any collision
-
+    // loop through the pattern on x and y-axis
     for (int i = 0; i < b.height; i++) {
         for (int j = 0; j < b.width; j++) {
+
             if (b.pattern[i][j] != 0) {
+
+                // gx, respectively gy stand for grid x/y
+                // these two are equal to the pivot plus j and respectively i
                 int gx = pivotX + j;
                 int gy = pivotY + i;
-                grid[gy][gx] = b.pattern[i][j]; // now safe to place
+                // place the block, this works by getting the cell in b.pattern, and placing it onto the grid at the
+                // coordinates calculated earlier
+                grid[gy][gx] = b.pattern[i][j];
             }
         }
     }
 }
 
 bool check_collision(int grid[8][8], block b, int pivotX, int pivotY) {
-    int coordX = pivotX;
-    int coordY = pivotY;
 
     for (int i = 0; i < b.height; i++) {
         for (int j = 0; j < b.width; j++) {
@@ -65,7 +77,9 @@ bool check_collision(int grid[8][8], block b, int pivotX, int pivotY) {
 
                 // collision or out-of-bounds
                 if (gx < 0 || gx >= 8 || gy < 0 || gy >= 8 || grid[gy][gx] != 0) {
-                    printf(RED "Can't place block at (X=%d;Y=%d)!\n" COLOR_RESET, coordX, coordY);
+                    // if the coordinate of the block on x-axis on the grid (gx) is less than 0 or >=  8 or gy < 0 or
+                    // gy >= 8 OR grid at position gx or gy is not 0, then not able to print and return false
+                    printf(RED "Can't place block at (X=%d;Y=%d)!\n" COLOR_RESET, pivotX, pivotY);
                     return false;
                 }
             }
@@ -76,24 +90,29 @@ bool check_collision(int grid[8][8], block b, int pivotX, int pivotY) {
 }
 
 void check_full_row(int grid[8][8]) {
+
+    // create coord struct
     typedef struct {
         int x;
         int y;
     } Coord;
 
+
+    // max size is 64 since that's the max size of cells that can be on the grid
     Coord coord[64];
     int clearCount = 0;
 
     // check rows
     for (int yAxis = 0; yAxis < MAX_GRID_HEIGHT; yAxis++) {
         int xAxisLine = 0;
-
+        // check columns
         for (int xAxis = 0; xAxis < MAX_GRID_WIDTH; xAxis++) {
             if (grid[yAxis][xAxis] == 1) {
                 xAxisLine++;
             }
         }
 
+        // mark coordinates in coord array
         if (xAxisLine == MAX_GRID_WIDTH) {
             printf("Row %d is full!\n", yAxis);
             for (int xAxis = 0; xAxis < MAX_GRID_WIDTH; xAxis++) {
@@ -107,13 +126,14 @@ void check_full_row(int grid[8][8]) {
     // check columns
     for (int xAxis = 0; xAxis < MAX_GRID_WIDTH; xAxis++) {
         int yAxisLine = 0;
-
+        // check rows
         for (int yAxis = 0; yAxis < MAX_GRID_HEIGHT; yAxis++) {
             if (grid[yAxis][xAxis] == 1) {
                 yAxisLine++;
             }
         }
 
+        //mark cell and store in coord array
         if (yAxisLine == MAX_GRID_HEIGHT) {
             printf("Column %d is full!\n", xAxis);
             for (int yAxis = 0; yAxis < MAX_GRID_HEIGHT; yAxis++) {
