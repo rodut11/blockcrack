3bd21e8b2d42aab18feda1bd912fb16e5cd3beab
Tataru Tudor Sorin <rodut11@protonmail.com>
2025-09-26 21:28:24 +0300

    created basic connect/disconnect functions for waydroid communication

---
+++ utils/waydroid/waydroid_comm.c
@@ -0,0 +1,5 @@
+//
+// Created by rodut11 on 9/26/25.
+//
+
+#include "waydroid_comm.h"
---
+++ utils/waydroid/waydroid_comm.h
@@ -0,0 +1,8 @@
+//
+// Created by rodut11 on 9/26/25.
+//
+
+#ifndef C_WAYDROID_COMM_H
+#define C_WAYDROID_COMM_H
+
+#endif //C_WAYDROID_COMM_H
